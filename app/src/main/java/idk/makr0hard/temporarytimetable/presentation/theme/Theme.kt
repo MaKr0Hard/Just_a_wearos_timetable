6abb90a20a0572e649f244ae179f4c2064e53a84
MaKr0Hard <258872817+MaKr0Hard@users.noreply.github.com>
2026-09-02 20:20:47 +0200

    crap

---
+++ app/src/main/java/idk/makr0hard/temporarytimetable/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package idk.makr0hard.temporarytimetable.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material3.MaterialTheme
+
+@Composable
+fun TemporaryTimetableTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
